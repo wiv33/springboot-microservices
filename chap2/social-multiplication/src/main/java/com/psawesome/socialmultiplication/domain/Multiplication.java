f5e2e5e5307580dec7e360a0fe10511a5c8b5f3b
PSK <wiv821253@gmail.com>
2020-01-26 15:36:04 +0900

    add Entity

--- chap2/social-multiplication/src/main/java/com/psawesome/socialmultiplication/domain/Multiplication.java
+++ chap2/social-multiplication/src/main/java/com/psawesome/socialmultiplication/domain/Multiplication.java
@@ -2,6 +2,11 @@ package com.psawesome.socialmultiplication.domain;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * package: com.psawesome.socialmultiplication.domain
  * author: PS
@@ -11,7 +16,13 @@ import lombok.*;
 @Getter
 @ToString
 @RequiredArgsConstructor
+@Entity
 public final class Multiplication {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MULTIPLICATION_ID")
+    private Long id;
 
     private final int factorA;
     private final int factorB;
