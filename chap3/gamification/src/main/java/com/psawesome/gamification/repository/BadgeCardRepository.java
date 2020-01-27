3d187f887eaddce3b001e419ae085becb3e41fc4
PSK <wiv821253@gmail.com>
2020-01-27 21:34:28 +0900

    add badgeRepository

---
+++ chap3/gamification/src/main/java/com/psawesome/gamification/repository/BadgeCardRepository.java
@@ -0,0 +1,21 @@
+package com.psawesome.gamification.repository;
+
+import com.psawesome.gamification.domain.BadgeCard;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * package: com.psawesome.gamification.repository
+ * author: PS
+ * DATE: 2020-01-27 월요일 21:31
+ */
+public interface BadgeCardRepository extends CrudRepository<BadgeCard, Long> {
+
+    /**
+     * @param userId
+     *         BadgeCard를 조회하고자 하는 사용자의 ID
+     * @return 최근 획득한 순으로 정렬된 BadgeCard 리스트
+     */
+    List<BadgeCard> findByUserIdOrderByBadgeTimestampDesc(final Long userId);
+}
