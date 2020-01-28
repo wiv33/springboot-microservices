d6b4bd0e315f71c0cf6ce1ca07ff715f3cf26e28
PSK <wiv821253@gmail.com>
2020-01-28 21:41:05 +0900

    GameStats, GameServiceImpl

--- chap3/gamification/src/main/java/com/psawesome/gamification/domain/GameStats.java
+++ chap3/gamification/src/main/java/com/psawesome/gamification/domain/GameStats.java
@@ -1,9 +1,51 @@
 package com.psawesome.gamification.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * package: com.psawesome.gamification.domain
  * author: PS
  * DATE: 2020-01-28 화요일 19:42
  */
-public class GameStats {
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class GameStats {
+
+    private final Long userId;
+    private final int score;
+    private final List<Badge> badges;
+
+    // JSON/JPA 를 위한 빈 생성자
+    public GameStats() {
+        this.userId = 0L;
+        this.score = 0;
+        this.badges = new ArrayList<>();
+    }
+
+    /**
+     * 빈 인스턴스(0점과 배지 없는 상태)를 만들기 위한 팩토리 메소드
+     *
+     * @param userId
+     *         사용자 ID
+     * @return {@link GameStats} 객체(0점과 배지 없는 상태)
+     */
+    public static GameStats emptyStats(final Long userId) {
+        return new GameStats(userId, 0, Collections.emptyList());
+    }
+
+    /**
+     * @return 수정불가능한 배지 카드 리스트의 뷰
+     */
+    public List<Badge> getBadges() {
+        return Collections.unmodifiableList(badges);
+    }
 }
--- chap3/gamification/src/main/java/com/psawesome/gamification/service/GameServiceImpl.java
+++ chap3/gamification/src/main/java/com/psawesome/gamification/service/GameServiceImpl.java
@@ -1,12 +1,19 @@
 package com.psawesome.gamification.service;
 
 import com.psawesome.gamification.client.MultiplicationResultAttemptClient;
+import com.psawesome.gamification.domain.Badge;
+import com.psawesome.gamification.domain.BadgeCard;
 import com.psawesome.gamification.domain.GameStats;
 import com.psawesome.gamification.domain.ScoreCard;
 import com.psawesome.gamification.repository.BadgeCardRepository;
 import com.psawesome.gamification.repository.ScoreCardRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * package: com.psawesome.gamification.service
@@ -34,12 +41,50 @@ public class GameServiceImpl implements GameService {
             scoreCardRepository.save(scoreCard);
             log.info("사용자 ID {}, 점수 {} 점, 답안 ID {}", userId, scoreCard.getScore(), attemptId);
 
-            processForBadges(userId, attemptId);
+            List<BadgeCard> badgeCards = processForBadges(userId, attemptId);
+            return new GameStats(userId, scoreCard.getScore(),
+                    badgeCards.stream()
+            .map(BadgeCard::getBadge)
+            .collect(Collectors.toList()));
         }
-        return null;
+        return GameStats.emptyStats(userId);
     }
 
-    private void processForBadges(Long userId, Long attemptId) {
+    private List<BadgeCard> processForBadges(Long userId, Long attemptId) {
+        List<BadgeCard> badgeCards = new ArrayList<>();
+        int totalScore = scoreCardRepository.getTotalScoreForUser(userId);
+        log.info("사용자 ID {}의 새로운 점수 {}", userId, totalScore);
 
+        List<ScoreCard> scoreCardList = scoreCardRepository.findByUserIdOrOrderByScoreTimestampDesc(userId);
+        List<BadgeCard> badgeCardList = badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId);
+        checkAndGiveBadgeBasedOnScore(badgeCardList, Badge.BRONZE_MULTIPLICATOR, totalScore, 100, userId).ifPresent(badgeCards::add);
+        checkAndGiveBadgeBasedOnScore(badgeCardList, Badge.SILVER_MULTIPLICATOR, totalScore, 500, userId).ifPresent(badgeCards::add);
+        checkAndGiveBadgeBasedOnScore(badgeCardList, Badge.GOLD_MULTIPLICATOR, totalScore, 999, userId).ifPresent(badgeCards::add);
+
+        if (scoreCardList.size() == 1 && !containsBadge(badgeCardList, Badge.FIRST_WON)) {
+            BadgeCard firstWonBadge = giveBadgeToUser(Badge.FIRST_WON, userId);
+            badgeCards.add(firstWonBadge);
+        }
+        return badgeCards;
     }
+
+    private BadgeCard giveBadgeToUser(Badge badge, Long userId) {
+        BadgeCard badgeCard = new BadgeCard(userId, badge);
+        badgeCardRepository.save(badgeCard);
+        log.info("사용자 ID {} 새로운 배지 획득: {}", userId, badge);
+        return badgeCard;
+    }
+
+    private boolean containsBadge(List<BadgeCard> badgeCards, Badge badge) {
+        return badgeCards.stream().anyMatch(b -> b.getBadge().equals(badge));
+    }
+
+    private Optional<BadgeCard> checkAndGiveBadgeBasedOnScore(List<BadgeCard> badgeCards, Badge badge, int score, int scoreThreshold, Long userId) {
+        if (score >= scoreThreshold && !containsBadge(badgeCards, badge)) {
+            return Optional.of(giveBadgeToUser(badge, userId));
+        }
+        return Optional.empty();
+    }
+
+
 }
