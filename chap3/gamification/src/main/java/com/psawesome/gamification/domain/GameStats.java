96cadbfc25d509744c65080d6b07a3fbe8e50b1c
PSK <wiv821253@gmail.com>
2020-01-28 19:44:55 +0900

    add GameStats, GameService

---
+++ chap3/gamification/src/main/java/com/psawesome/gamification/domain/GameStats.java
@@ -0,0 +1,9 @@
+package com.psawesome.gamification.domain;
+
+/**
+ * package: com.psawesome.gamification.domain
+ * author: PS
+ * DATE: 2020-01-28 화요일 19:42
+ */
+public class GameStats {
+}
---
+++ chap3/gamification/src/main/java/com/psawesome/gamification/service/GameService.java
@@ -0,0 +1,24 @@
+package com.psawesome.gamification.service;
+
+import com.psawesome.gamification.domain.GameStats;
+
+/**
+ * package: com.psawesome.gamification.service
+ * author: PS
+ * DATE: 2020-01-28 화요일 19:42
+ */
+public interface GameService {
+
+    /**
+     * 주어진 사용자가 제출한 답안을 처리
+     *
+     * @param userId
+     *         사용자 ID
+     * @param attemptId
+     *         필요한 경우 추가로 데이터를 조회하기 위한 답안 ID
+     * @param correct
+     *         답안의 정답 여부
+     * @return 새로운 점수와 배지 카드를 포함한 {@link GameStats} 객체
+     */
+    GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct);
+}
