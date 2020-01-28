bcfc424841fe36fbcca9d7a05ccea385e777a1fe
PSK <wiv821253@gmail.com>
2020-01-28 21:42:12 +0900

    create MultiplicationResultAttemptClientImpl

---
+++ chap3/gamification/src/main/java/com/psawesome/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -0,0 +1,15 @@
+package com.psawesome.gamification.client;
+
+import com.psawesome.gamification.client.dto.MultiplicationResultAttempt;
+
+/**
+ * package: com.psawesome.gamification.client
+ * author: PS
+ * DATE: 2020-01-28 화요일 21:41
+ */
+public class MultiplicationResultAttemptClientImpl implements MultiplicationResultAttemptClient {
+    @Override
+    public MultiplicationResultAttempt retrieveMultiplicationResultAttemptById(Long multiplicationId) {
+        return null;
+    }
+}
