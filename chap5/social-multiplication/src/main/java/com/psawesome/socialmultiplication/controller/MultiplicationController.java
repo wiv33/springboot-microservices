6cd1c22752ba30c6da66188baf18dfb0f52dcec5
PSK <wiv821253@gmail.com>
2020-02-02 20:17:59 +0900

    Round robbin check

--- chap5/social-multiplication/src/main/java/com/psawesome/socialmultiplication/controller/MultiplicationController.java
+++ chap5/social-multiplication/src/main/java/com/psawesome/socialmultiplication/controller/MultiplicationController.java
@@ -2,29 +2,39 @@ package com.psawesome.socialmultiplication.controller;
 
 import com.psawesome.socialmultiplication.domain.Multiplication;
 import com.psawesome.socialmultiplication.service.MultiplicationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 /**
  * package: com.psawesome.socialmultiplication.controller
  * author: PS
  * DATE: 2020-01-22 수요일 23:54
  */
+@Slf4j
 @RestController
 @RequestMapping("/multiplication")
 public final class MultiplicationController {
 
-    private MultiplicationService service;
+    private final MultiplicationService service;
+    private final int serverPort;
 
     @Autowired
-    public MultiplicationController(final MultiplicationService service) {
+    public MultiplicationController(final MultiplicationService service,
+                                    final @Value("${server.port}") int serverPort) {
         this.service = service;
+        this.serverPort = serverPort;
     }
 
     @GetMapping("/random")
     public Multiplication getRandomMultiplication() {
+        log.info("Running Multiplication Server port {}, Time: {}", serverPort, LocalDateTime.now(Clock.systemDefaultZone()));
         return service.createRandomMultiplication();
     }
 }
