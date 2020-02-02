21f01603c2ae9c1516f79755ad55cec8e0b61bc6
PSK <wiv821253@gmail.com>
2020-02-02 15:14:43 +0900

    update eureka-client

--- chap5/zuul-gateway/src/main/java/com/psawesome/zuulgateway/ZuulGatewayApplication.java
+++ chap5/zuul-gateway/src/main/java/com/psawesome/zuulgateway/ZuulGatewayApplication.java
@@ -2,9 +2,11 @@ package com.psawesome.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
