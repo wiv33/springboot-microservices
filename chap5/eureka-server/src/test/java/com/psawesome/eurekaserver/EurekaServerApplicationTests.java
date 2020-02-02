442212c319f9ad34b3f78c47ed2b5035c967eaa3
PSK <wiv821253@gmail.com>
2020-02-02 14:44:54 +0900

    add eureka-server

---
+++ chap5/eureka-server/src/test/java/com/psawesome/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psawesome.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
