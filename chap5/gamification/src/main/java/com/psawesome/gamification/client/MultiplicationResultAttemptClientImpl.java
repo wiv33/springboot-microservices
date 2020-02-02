72a948173219d26ae29ebb4dd70cba377dec1ac5
PSK <wiv821253@gmail.com>
2020-02-02 23:01:56 +0900

    게임화 서비스에 hystrix 로직 추가

--- chap5/gamification/src/main/java/com/psawesome/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ chap5/gamification/src/main/java/com/psawesome/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,6 @@
 package com.psawesome.gamification.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.psawesome.gamification.client.dto.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -22,8 +23,13 @@ public class MultiplicationResultAttemptClientImpl implements MultiplicationResu
     }
 
     // 해당 메서드는 시간차로 인해 100% 안전하지 않다.
+    @HystrixCommand(fallbackMethod = "defaultResult")
     @Override
     public MultiplicationResultAttempt retrieveMultiplicationResultAttemptById(Long multiplicationResultAttemptId) {
         return restTemplate.getForObject(multiplicationHost + "/results/{resultId}", MultiplicationResultAttempt.class, multiplicationResultAttemptId);
+    }
+
+    private MultiplicationResultAttempt defaultResult(final Long multiplicationResultAttemptId) {
+        return new MultiplicationResultAttempt("fakeAlias", 10, 10, 100, true);
     }
 }
